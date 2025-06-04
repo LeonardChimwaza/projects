59758643c6e44bad78ae41f1c5e4d6c76d9b6b31
leonard chimwaza <bsc-25-23@unima.ac.mw>
2025-06-04 06:45:13 -0700

    Initial commit - Added project9

---
+++ project9/polygon/Polygon.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Polygon.h"
+
+void Polygon::setValues(int width, int height) {
+    mWidth = width;
+    mHeight = height;
+}
+
+void Polygon::PrintArea() {
+    cout << this->PrintArea() << '\n';
+}
+
+
